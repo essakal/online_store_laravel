0ce8800126cfb68a9a8e21f59311793806b64b67
essakal <essakal.msi@gmail.com>
2023-05-02 23:02:42 +0100

    kui

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
