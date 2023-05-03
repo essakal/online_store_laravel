58ff448c4334dda5598e7d0a805d5c450d763042
essakal <essakal.msi@gmail.com>
2023-05-03 19:42:08 +0100

    logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -181,8 +181,15 @@
                                 aria-labelledby="dropdownMenuButton2" data-te-dropdown-menu-ref>
                                 <!-- Second dropdown menu items -->
                                 <li>
-                                    <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-                                        href="#" data-te-dropdown-item-ref>Action</a>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a href="{{ route('logout') }}"
+                                            class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                                            onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </a>
+                                    </form>
                                 </li>
                                 <li>
                                     <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
