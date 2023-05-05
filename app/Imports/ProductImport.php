a5f433986b49ac1a57243993df5c8313e020d52b
essakal <essakal.msi@gmail.com>
2023-05-05 20:06:59 +0100

    import excel and check category

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -2,12 +2,11 @@
 
 namespace App\Imports;
 
+use App\Models\Category;
 use App\Models\Produit;
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Hash;
-use Illuminate\Support\Facades\Hash as FacadesHash;
 
 class ProductImport implements ToModel, WithHeadingRow
 {
@@ -18,12 +17,27 @@ class ProductImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        $category = Category::where('name', $row['category'])->first();
+
+        if ($category) {
+            // if category exists, set its ID in the 'category_id' field of the Product
+            $category_id = $category->id;
+        } else {
+            // if category does not exist, create a new Category record and set its ID in the 'category_id' field of the Product
+            $category = new Category([
+                'name' => $row['category']
+            ]);
+            $category->save();
+            $cat = Category::where('name', $row['category'])->first();
+            $category_id = $cat->id;
+        }
         return new Produit([
             'name' => $row['name'],
             'description' => $row['description'],
             'prix' => $row['prix'],
             'quantité' => $row['quantite'],
-            'category_id' => $row['category'],
+            // 'category_id' => $row['category'],
+            'category_id' => $category_id,
             'image' => '123.png',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
