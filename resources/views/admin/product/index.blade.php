3e7ffcb1541407530cfed1ca9b68a5a21d5da6df
essakal <essakal.msi@gmail.com>
2023-05-05 01:00:12 +0100

    start excel

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -5,8 +5,12 @@
     </h3>
     <div class="">
         <a href="{{ route('admin.products.create') }}"
-            class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            class="mr-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             add new produit
+        </a>
+        <a href="#"
+            class="mr-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            upload new produits
         </a>
     </div>
     @if ($message = Session::get('success'))
